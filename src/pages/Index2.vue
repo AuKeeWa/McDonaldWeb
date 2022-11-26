e1cbfc32207ddcea1c2c4a85462d1cd4afab7477
Emerina <903729210@qq.com>
2022-11-26 21:55:51 +0800

    edit style from index.vue unfinished

--- src/pages/Index2.vue
+++ src/pages/Index2.vue
@@ -2,209 +2,373 @@
     <div class="index">
         <!-- 首页广告 -->
         <div class="home" id="home">
+            ::before
             <div class="ads" id="ads">
                 <div class="col-md-6 col-sm-8 col-xs-12 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/00_640_398.png">
                     <div class="info">
 				        <h4>1元1块！开赛！！！！</h4>
-			        </div>
+                    </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/01_640_480.jpg">
                     <div class="info">
 				        <h4>巨无霸免费送！还送满减券！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/02_640_480.jpg">
                     <div class="info">
 				        <h4>免费！周二继续喝奶铁！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/03_640_480.jpg">
                     <div class="info">
 				        <h4>请吃早餐你冲不冲？冲冲冲</h4>
 			        </div>
                 </div>
-                <div class="col-md-3 col-sm-4 col-xs-12 block">
+                <div class="col-md-3 col-sm-4 col-xs-6 block">
                     <img src="././public/images/index/04.gif">
                 </div>
                 <div class="col-md-6 col-sm-8 col-xs-12 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/05_640_398.jpg">
                     <div class="info">
 				        <h4>@你，0元辣翅已送达！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/06_640_480.jpg">
                     <div class="info">
 				        <h4>6折！粥王系列加入！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/07_640_480.jpg">
                     <div class="info">
 				        <h4>厚椰来了！免费多1杯！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/08_640_480.png">
                     <div class="info">
 				        <h4>脆皮甜心：第2份半价爱你！</h4>
 			        </div>
                 </div>
                 <div class="col-md-6 col-sm-8 col-xs-12 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/09_640_398.png">
                     <div class="info">
 				        <h4>新！巨无霸！8.8元限量抢！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/10_640_480.jpg">
                     <div class="info">
 				        <h4>麦当劳中国推出新升级巨无霸，与乒乓球世界冠军马龙一起打卡吧！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/11_640_480.jpg">
                     <div class="info">
 				        <h4>买1送1！快告诉你的“奶铁”！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/12_640_480.jpg">
                     <div class="info">
 				        <h4>减5元！周一chill一下</h4>
 			        </div>
                 </div>
                 <div class="col-md-6 col-sm-8 col-xs-12 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/13_640_398.png">
                     <div class="info">
 				        <h4>0元！1元！今日羊毛今日薅</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/14_640_480.jpg">
                     <div class="info">
 				        <h4>奶铁日！免费多1杯！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/15_640_480.jpg">
                     <div class="info">
 				        <h4>3元薯条来袭，今天来？</h4>
 			        </div>
                 </div>
                 <div class="col-md-6 col-sm-8 col-xs-12 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/16_640_398.jpg">
                     <div class="info">
 				        <h4>6.5元板烧，开启双11狂欢！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/17_640_480.jpg">
                     <div class="info">
 				        <h4>早上就要喝现磨！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/18_640_480.jpg">
                     <div class="info">
 				        <h4>立减5元！预祝下班快乐！</h4>
 			        </div>
                 </div>
                 <div class="col-md-6 col-sm-8 col-xs-12 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/19_640_398.jpg">
                     <div class="info">
 				        <h4>0运费，0餐费，0元桶！快来抽！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/20_640_480.png">
                     <div class="info">
 				        <h4>红糖冰粉来啦！奶黄派返场！</h4>
 			        </div>
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/21_640_480.jpg">
                     <div class="info">
 				        <h4>随心配升级！+2元！限时新品快来尝！</h4>
 			        </div>
                 </div>
                 <div class="col-md-6 col-sm-8 col-xs-12 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/22_640_398.jpg">
                     <div class="info">
 				        <h4>全方位安全服务，麦麦只为你的放心</h4>
 			        </div>
                 </div>
                 <div class="col-md-6 col-sm-8 col-xs-12 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/23_640_398.jpg">
                     <div class="info">
                 		<h4>5元汉堡来啦，“零碳”餐厅也来啦！</h4>
                 	</div>
                 </div>
-                <div class="col-md-3 col-sm-4 col-xs-12 block">
+                <div class="col-md-3 col-sm-4 col-xs-6 block">
                     <img src="././public/images/index/24_640_829.jpg">
                 </div>
-                <div class="col-md-3 col-sm-4 col-xs-12 block">
+                <div class="col-md-3 col-sm-4 col-xs-6 block">
                     <img src="././public/images/index/25.gif">
                 </div>
                 <div class="col-md-3 col-sm-4 col-xs-6 block">
-                    <span class="tag"><i class="iconfont">&#xe60b;</i></span>
                     <img src="././public/images/index/26_640_480.png">
                     <div class="info">
 				        <h4>谁是“芋泥”脑袋，点进来瞧瞧！</h4>
 			        </div>
                 </div>
-                <div class="col-md-3 col-sm-4 col-xs-12 block">
+                <div class="col-md-3 col-sm-4 col-xs-6 block">
                     <img src="././public/images/index/27_640_829.jpg">
                 </div>
-                <div class="col-md-3 col-sm-4 col-xs-12 block">
+                <div class="col-md-3 col-sm-4 col-xs-6 block">
                     <img src="././public/images/index/28.gif">
                 </div>
-                <div class="col-md-3 col-sm-4 col-xs-12 block">
+                <div class="col-md-3 col-sm-4 col-xs-6 block">
                     <img src="././public/images/index/29_640_829.jpg">
                 </div>
-                <div class="col-md-3 col-sm-4 col-xs-12 block">
+                <div class="col-md-3 col-sm-4 col-xs-6 block">
                     <img src="././public/images/index/30_640_829.jpg">
                 </div>
-                <div class="col-md-3 col-sm-4 col-xs-12 block">
+                <div class="col-md-3 col-sm-4 col-xs-6 block">
                     <img src="././public/images/index/31.gif">
                 </div>
-                <div class="col-md-3 col-sm-4 col-xs-12 block">
+                <div class="col-md-3 col-sm-4 col-xs-6 block">
                     <img src="././public/images/index/32.gif">
                 </div>
             </div>
+            ::after
         </div>
     </div>
-  </template>
+</template>
   
-  <script>
-  </script>
+<script>
+</script>
   
-  <style lang='scss' scoped>
-  </style>
+<style lang='scss' scoped>
+    * {
+        -webkit-box-sizing:border-box;
+        -moz-box-sizing:border-box;
+        box-sizing:border-box
+    }
+    :after,
+    :before {
+        -webkit-box-sizing:border-box;
+        -moz-box-sizing:border-box;
+        box-sizing:border-box
+    }
+    .container {
+        padding-right:15px;
+        padding-left:15px;
+        margin-right:auto;
+        margin-left:auto
+    }
+    @media (min-width:768px) {
+        .container {
+            width:750px
+        }
+    }
+    @media (min-width:992px) {
+        .container {
+            width:970px
+        }
+    }
+    .index {
+        display: block;
+        position: relative;
+        width: 100%;
+        padding: 40px 0 90px 0;
+    }
+    @media (max-width: 767px){
+        .index {
+            padding: 0 0 70px 0 !important;
+        }
+    }
+    .index {
+        min-height: calc(100vh - 120px - 290px);
+    }
+    .home {
+        max-width: 1160px;
+        margin: 0 -10px 0 -10px;
+        padding: 0 20px;
+    }
+
+    .home .block {
+        position: relative;
+        margin: 0 0 20px 0;
+        padding: 0 10px 0 10px;
+        overflow: hidden;
+    }
+    .ads{
+        position: relative;
+        height: 0px;
+    }
+    .ads::before{
+        display: table;
+        content: " ";
+    }
+    .ads::after{
+        clear: both;
+    }
+    .home .ads{
+        margin: 0 10px 0 10px;
+    }
+    .col-md-3,
+    .col-md-6,
+    .col-sm-4,
+    .col-sm-8,
+    .col-xs-12,
+    .col-xs-6 {
+        position:relative;
+        min-height:1px;
+        padding-right:15px;
+        padding-left:15px
+    }
+    .col-xs-12,
+    .col-xs-6 {
+        float:left
+    }
+    .col-xs-12 {
+        width:100%
+    }
+    .col-xs-6 {
+        width:50%
+    }
+    @media (min-width:768px) {
+        .col-sm-4,
+        .col-sm-8 {
+            float:left
+        }
+        .col-sm-8 {
+            width:66.66666667%
+        }
+        .col-sm-4 {
+            width:33.33333333%
+        }
+    }
+    @media (min-width:992px) {
+        .col-md-3,
+        .col-md-6 {
+            float:left
+        }
+        .col-md-6 {
+            width:50%
+        }
+        .col-md-3 {
+            width:25%
+        }
+    }
+    .home .block {
+        position: relative;
+        margin: 0 0 20px 0;
+        padding: 0 10px 0 10px;
+        overflow: hidden;
+    }
+    
+    .home .col-md-3 .col-sm-4 {
+        display: block;
+        position: relative;
+        height: 343px;
+        background: #ffbc0d;
+    }
+    .home .col-md-6 .col-sm-8 {
+        display: block;
+        position: relative;
+        height: 343px;
+        background: #ffbc0d;
+    }
+    @media (max-width: 767px) {
+        .col-md-3.col-sm-4.col-xs-6 {
+            height: 221px!important;
+        }
+    }
+    .block {
+        position: relative;
+        margin: 0 0 20px 0;
+        padding: 0 10px 0 10px;
+        overflow: hidden;
+    }
+    .home .block > img {
+        border: 0;
+        display: block;
+        width: 100%;
+        height: auto;
+        overflow: hidden;
+        cursor: pointer;
+    }
+    .home .col-md-6.col-sm-8 .block > img{
+        height: 100%;
+    }
+    .home .col-md-6 .col-sm-8 .info {
+        display: block;
+        position: absolute;
+        width: 100%;
+        left: 0;
+        bottom: 0;
+        padding: 17px;
+        background: rgba(0, 0, 0, 0.3);
+        box-sizing: border-box;
+    }
+    .home .col-md-6 .col-sm-8 .info h4 {
+        max-height: 60px;
+        margin-bottom: 15px;
+        font-family: inherit;
+        font-size: 22px;
+        line-height: 1.4;
+        color: #fff;
+        font-weight: 800;
+    }
+    .home .col-md-3 .col-sm-4 .info {
+        display: block;
+        padding: 20px;
+    }
+    .home .col-md-3 .col-sm-4 .info h4 {
+        max-height: 69px;
+        margin-bottom: 15px;
+        font-family: inherit;
+        font-size: 18px;
+        line-height: 1.3;
+        color: #000;
+        font-weight: 800;
+    }
+    
+</style>
